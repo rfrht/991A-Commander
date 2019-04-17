e88b712656e509109cf122fe17f1bddaa6ffbbf2
Rodrigo Freire <rbs@brasilia.br>
2019-04-17 13:30:17 -0300

    Serial Autoconfig
    - Set date & Time
    - New function (for silent stuff)
    - Serial autoconfig

--- prototype.sh
+++ prototype.sh
@@ -1,30 +1,69 @@
 #!/bin/bash
 DEVICE=/dev/ttyUSB0
 
-config_device(){
+serial_config(){
+	# Bail out if there was a failed attempt after turning on the radio
+	if [ $RETRY == 1 ] 2>/dev/null ; then
+		echo "After waking up the radio, it is still not responding. Ending."
+		exit 1
+	fi
+
+	# Identify the serial port
+	echo "Configuring Serial Port & waking up the radio"
 	DEVICE=$(for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do \
 		(syspath="${sysdevpath%/dev}" ; devname="$(udevadm info -q name -p $syspath)" ; \
 		[[ "$devname" == "bus/"* ]] && continue ; eval "$(udevadm info -q property --export -p $syspath)" ; \
 		[[ -z "$ID_SERIAL" ]] && continue ; echo "/dev/$devname - $ID_SERIAL" ; ) ; \
 		done | grep -m 1 CP2105 | awk '{print $1}')
 
+	# Case no device is found...
 	if [ -z $DEVICE ] ; then
 	   echo "FT-991A not connected"
    	   exit 1
 	fi
 
-stty -F $DEVICE 38400
+	echo "Identified device $DEVICE"
+
+	# Set port speed
+	if ! stty -F $DEVICE 38400 ; then
+		echo "Failed to configure port, retrying in 20s" 
+		sleep 20
+		if ! stty -F $DEVICE 38400 ; then
+			echo "Failed to config serial port"
+			exit 1
+		fi
+	else
+	# Check for power state
+		send_silent PS
+		sleep 1
+		send_command PS
+		if [ ${OUTPUT:2} != 1 ] ; then 
+			echo "Waking up the radio..."
+			send_silent PS1
+			RETRY=1
+			sleep 7
+		fi
+	# Sync radio clock
+	echo "Configuring radio Clock"
+	DATE=$(TZ=UTC date +"%Y%m%d")
+	TIME=$(TZ=UTC date +"%H%M%S")
+	send_silent DT0$DATE
+	send_silent DT1$TIME
+	sleep 2
+	fi
 }
 
+send_silent() {
+	echo -n "$1;" > $DEVICE
+}
 
 send_command() {
 	echo -n "$1;" > $DEVICE &
-	read -d ';' OUTPUT <$DEVICE
+	read -t 1 -d ';' OUTPUT <$DEVICE || serial_config
 }
 
 get_ptt() {
-	send_command RIA
-	XMIT_STATE=${OUTPUT:3}
+	send_command RIA ; XMIT_STATE=${OUTPUT:3}
 	case $XMIT_STATE in
 	0) send_command BY ;
 		case ${OUTPUT:2:1} in
@@ -45,6 +84,7 @@ get_qrg() {
 	ST_RX_SRC_ID=${OUTPUT:22:1}
 	ST_TONE_ID=${OUTPUT:23:1}
 	ST_SHIFT_ID=${OUTPUT:26:1}
+
 	case $ST_SHIFT_ID in
 	0) ST_SHIFT_TYPE=Simplex ;;	1) ST_SHIFT_TYPE="+" ;;
 	2) ST_SHIFT_TYPE="-" ;;
@@ -76,32 +116,32 @@ get_qrg() {
 	case ${OUTPUT:2} in
 	0) ST_LOCK=Unlocked ;;	1) ST_LOCK=Locked
 	esac
+
+	case $ST_RX_SRC_ID in 1) 
+	send_command MT$ST_MCHAN ; ST_MTAG=${OUTPUT:28} ;;
+	esac
 }
 
 get_smeter() {
-	send_command SM0
-	ST_SMETER=${OUTPUT:3}
+	send_command SM0 ; ST_SMETER=${OUTPUT:3}
 }
 
 get_txdata() {
-	send_command RM3
-	ST_COMP=${OUTPUT:3}
-	send_command RM4
-	ST_ALC=${OUTPUT:3}
-	send_command RM5
-	ST_PO=${OUTPUT:3}
-	send_command RM6
-	ST_SWR=${OUTPUT:3}
-	send_command RM7
-	ST_IDD=${OUTPUT:3}
-	send_command RM8
-	ST_VDD=${OUTPUT:3}
+	send_command RM3 ; ST_COMP=${OUTPUT:3}
+	send_command RM4 ; ST_ALC=${OUTPUT:3}
+	send_command RM5 ; ST_PO=${OUTPUT:3}
+	send_command RM6 ; ST_SWR=${OUTPUT:3}
+	send_command RM7 ; ST_IDD=${OUTPUT:3}
+	send_command RM8 ; ST_VDD=${OUTPUT:3}
 }
 
 print_header(){
-	echo "Source VFO: $ST_VFO_SOURCE | QRG: $(echo $ST_VFOA | numfmt  --suffix=Hz --grouping) | $(if [ $ST_RX_SRC_ID == 1 ] ; then echo Memory: $ST_MCHAN \|; fi) Mode: $ST_MODE | State: $ST_TX"
+	echo "Source VFO: $ST_VFO_SOURCE | QRG: $(echo $ST_VFOA | numfmt  --suffix=Hz --grouping) | Mode: $ST_MODE | State: $ST_TX"
 	if [[ $ST_TONE_ID != 0 && $ST_MODE_ID == 4 ]] ; then
-		 echo "Tone/DCS: $ST_TONE_TYPE | RPT Shift: $ST_SHIFT_TYPE "
+		 echo -n "Tone/DCS: $ST_TONE_TYPE | RPT Shift: $ST_SHIFT_TYPE "
+	fi
+	if [ $ST_RX_SRC_ID == 1 ] ; then 
+		echo "| Memory: $ST_MCHAN | Name: $ST_MTAG"
 	fi
 	echo "VFO Lock: $ST_LOCK | Clarifier: $ST_CLAR | RX Clar: $ST_RX_CLAR | TX Clar: $ST_TX_CLAR"
 }
@@ -121,7 +161,7 @@ print_tx(){
 while true ; do
 	get_ptt
 
-	if [ $XMIT_STATE == 0 ] ; then
+	if [ $XMIT_STATE == 0 ] 2>/dev/null ; then
 		get_qrg
 		get_smeter
 		print_rx
@@ -130,5 +170,5 @@ while true ; do
 		get_txdata
 		print_tx
 	fi
-	sleep .4
+	sleep 0.5
 done
